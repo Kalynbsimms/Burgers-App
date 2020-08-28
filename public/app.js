76bf1dccb8bd2ac8dfe792ef8ee548d45e649a82
Kalyn Simms <Kalynbsimms@gmail.com>
2020-08-28 09:52:22 -0700

    added axios cdn

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
 document.getElementById('addBurger').addEventListener('click', event => {
     event.preventDefault()
+    console.log('ping')
 
     axios.post('/api/burgers', {
-        name: document.getElementById('product').value,
-        purchased: false
+        name: document.getElementById('burger').value,
+        devoured: false
     })
         .then(({ data }) => {
             let burgerElem = document.createElement('li')
@@ -11,26 +12,26 @@ document.getElementById('addBurger').addEventListener('click', event => {
             burgerElem.id = data.id
             burgerElem.innerHTML = `
        <div class="d-flex w-100 justify-content-between">
-         <h5 class="mb-1">${document.getElementById('product').value}</h5>
+         <h5 class="mb-1">${document.getElementById('burger').value}</h5>
          <button 
-          data-name="${document.getElementById('product').value}"
+          data-name="${document.getElementById('burger').value}"
        
-          class="purchase btn btn-success">✓</button>
+          class="devour btn btn-success">✓</button>
        </div>
  
       `
             document.getElementById('notDevoured').append(burgerElem)
 
-            document.getElementById('product').value = ''
+            document.getElementById('burger').value = ''
            
         })
         .catch(err => console.error(err))
 })
 
 document.addEventListener('click', event => {
-    if (event.target.classList.contains('purchase')) {
+    if (event.target.classList.contains('devour')) {
         axios.put(`/api/burgers/${event.target.parentNode.parentNode.id}`, {
-            purchased: true
+            devoured: true
         })
             .then(() => {
                 let burgerElem = document.createElement('li')
@@ -43,7 +44,7 @@ document.addEventListener('click', event => {
        </div>
        
       `
-                document.getElementById('purchased').append(burgerElem)
+                document.getElementById('devoured').append(burgerElem)
                 event.target.parentNode.parentNode.remove()
             })
             .catch(err => console.error(err))
